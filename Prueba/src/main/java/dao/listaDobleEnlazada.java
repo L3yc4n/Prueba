d174cd51966ce2fef0cb558e5c505197ea2f176a
Ariana <ariana.nikol30@gmail.com>
2025-05-25 14:03:29 -0500

    Agregue mi nombre Ariana

--- Prueba/src/main/java/dao/listaDobleEnlazada.java
+++ Prueba/src/main/java/dao/listaDobleEnlazada.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+import dto.Nodo;
+import dto.Alumno;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Enrique
- */
 public class listaDobleEnlazada {
+    Nodo cabeza;
+
+    public listaDobleEnlazada() {
+        this.cabeza = null;
+    }
     
-}
+    public void agregar(Alumno alumno){
+    Nodo nuevonodo=new Nodo(alumno);
+    if(cabeza==null){
+        cabeza=nuevonodo;
+    } else {
+        Nodo actual=cabeza;
+        while(actual.getSiguiente()!=null){
+            actual=actual.getSiguiente();
+        }
+        actual.setSiguiente(nuevonodo);
+        nuevonodo.setSiguiente(actual);
+    }
+    
+    }
+    
+    public void mostrar (Alumno alumno){
+        if(cabeza==null){
+            JOptionPane.showMessageDialog(null, "que vas ps, si no hay nada");
+            return;
+        }
+      Nodo actual=cabeza;
+        SimpleDateFormat sdf =new SimpleDateFormat ("dd/mm/yy");
+        String resultado="alumno: \n";
+        resultado +="codigo-nombre-nota-nacimiento\n";
+        while(actual!=null){
+            Alumno a= (Alumno)actual.getDato();
+           resultado += a.getCodigo()+" - "+a.getNombre()+" - "+a.getNota()+" - "+ sdf.format(a.getNacimiento())+"\n";
+           actual=actual.getSiguiente();
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+    
+}//FINAL
--- Prueba/src/main/java/dto/Alumno.java
+++ Prueba/src/main/java/dto/Alumno.java
@@ -1,17 +1,29 @@
 package dto;
 
+import java.util.Date;
+
 public class Alumno {
     private int codigo;
     private String nombre;
     private double nota;
+    private Date nacimiento;
 
-    public Alumno(int codigo, String nombre, double nota) {
+    public Alumno(int codigo, String nombre, double nota,Date nacimiento) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.nota = nota;
+        this.nacimiento=nacimiento;
     }
 
     public Alumno() {
+    }
+
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
     }
 
     public int getCodigo() {
