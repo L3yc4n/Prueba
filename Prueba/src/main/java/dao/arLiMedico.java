53afe7ead846c527193932879ae3841a1ed1d151
Ariana <ariana.nikol30@gmail.com>
2025-05-25 22:44:06 -0500

    Agregue mi nombre Ariana

--- Prueba/src/main/java/dao/arLiMedico.java
+++ Prueba/src/main/java/dao/arLiMedico.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author Enrique
- */
+import dto.Medico;
+import java.util.ArrayList;
+import java.util.List;
+
 public class arLiMedico {
     
+    private List<Medico> lista =new ArrayList<>();
+    
+    private int ubi=-1;
+    public boolean lista_vacia(){
+        return lista.isEmpty();
+    }
+    public void primero(){
+        ubi=0;
+    }
+    public void ultimo(){
+        ubi=lista.size()-1;
+    }
+    public void siguiente(){
+        if(ubi<lista.size()-1){
+            ubi++;
+        }
+    }
+    public void anterior(){
+        if(ubi>0){
+            ubi--;
+        }
+    }
+    
+    //modificar
+    public Medico getMedico(){
+        return lista.get(ubi);
+    }
+    public void setMedico(Medico m){
+        lista.set(ubi, m);
+    }
+    public void agregar(Medico m){
+        lista.add(m);
+        ubi=lista.size()-1;
+    }
+    public String imprimir(){
+        String imprimir=" ";
+        for (Medico medico : lista) {
+            imprimir += medico.getCodigo()+" - "+medico.getNombre()+" - "+medico.getApellido()+" - "+medico.getNacimiento()+"\n";
+        }
+        return imprimir;
+    }
+    public void eliminar(){
+        if(!lista.isEmpty()){
+            lista.remove(ubi);
+            if(ubi>=lista.size()){
+                ubi=lista.size()-1;
+            }
+        }
+    }
 }
